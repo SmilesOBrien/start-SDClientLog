e8ceb3f4875e0e24c0a29ea794197d1b5d24def9
smiles-obrien <109524183+smiles-obrien@users.noreply.github.com>
2024-09-20 11:28:19 -0400

    Create start-SDClientLog.psm1
    Version of script that can be imported to your modules folder, allowing it to be called directly from powershell instead of as a standalone script

---
+++ start-SDClientLog.psm1
@@ -0,0 +1,28 @@
+<#
+    .SYNOPSIS
+        An easy way to view SmartDeploy Client Service Logs, for folks who don't want to keep looking up the log file or typing in the commands.
+
+    .DESCRIPTION
+        An easy way to view SmartDeploy Client Service Logs, for folks who don't want to keep looking up the log file or typing in the commands.
+
+    #>
+
+Function start-sdclientlog () {
+    $Title = "============== SmartDeploy Powershell Log Viewer ==============="
+    $Prompt = "Is this the local computer, or a remote computer?"
+    $Choices = [System.Management.Automation.Host.ChoiceDescription[]] @("&Remote", "&Local")
+    $Default = 0
+
+    $Choice = $host.UI.PromptForChoice($Title, $Prompt, $Choices, $Default)
+
+    Switch($Choice) {
+        0 {
+                $Computer = Read-Host -Prompt "What is the computer name?"
+                get-content "\\$Computer\c$\program files\smartdeploy\clientservice\sdclientservice.log" -tail 50 -Wait
+        }
+        1 {
+                get-content "C:\Program Files\SmartDeploy\ClientService\SDClientService.log" -tail 50 -Wait
+
+            }
+    }
+}
